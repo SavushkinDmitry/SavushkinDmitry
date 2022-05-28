8ee351b5f9db363e20934edd457d6b153936b2f0
Savushkin Dmitry <wexler.kill@mail.ru>
2022-05-29 00:43:33 +0300

    Fix project

--- seleniumAvito/src/main/java/AvitoSelen.java
+++ seleniumAvito/src/main/java/AvitoSelen.java
@@ -3,8 +3,6 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.util.List;
@@ -14,6 +12,8 @@ public class AvitoSelen {
         System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
+        SelectCategory selectCategory = new SelectCategory(driver);
+        SelectFilter filter = new SelectFilter(driver);
 
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(8));
@@ -23,15 +23,7 @@ public class AvitoSelen {
 
         //Шаг 2
         //Ищем категории по id
-        Select select = new Select(driver.findElement(By.cssSelector("#category")));
-
-        //Выводим все найденые элементы
-        select.getOptions().forEach(webElement -> {
-            System.out.println(webElement.getText());
-        });
-
-        //Выбираем категорию
-        select.selectByVisibleText("Оргтехника и расходники");
+        selectCategory.setCategory("Оргтехника и расходники");
 
         //Шаг 3
         //Ищем поле ввода и вписываем "Принтер"
@@ -40,20 +32,17 @@ public class AvitoSelen {
         //Шаг 4
         //Ищем элемент выбора региона и кликаем по нему
         driver.findElement(By.xpath("//div[@data-marker='search-form/region']")).click();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
+        //Шаг 5
         //В новом окне ищем поле вывода региона и вписываем "Владивосток"
         driver.findElement(By.xpath("//div/input[@data-marker='popup-location/region/input']")).sendKeys("Владивосток");
 
-        //Шаг 5
         //Ищем первый элемент из выпадающего списка и кликаем по нему
-        WebElement searchRegion = new WebDriverWait(driver, Duration.ofSeconds(3))
-                .until(webDriver -> driver.findElement(By.xpath("//li[@data-marker='suggest(0)']")));
+        WebElement searchRegion = driver.findElement(By.xpath("//li[@data-marker='suggest(0)']"));
         System.out.println(searchRegion.getText());
         searchRegion.click();
 
         driver.findElement(By.xpath("//button[@data-marker='popup-location/save-button']")).click();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
         //Шаг 6
         //Ищем checkbox и делаем проверку, если неактивне, то кликаем по нему и нажимаем кнопку
@@ -65,11 +54,7 @@ public class AvitoSelen {
 
         //Шаг 7
         //Ищем элементы фильтра и выбираем соответствующее значение
-        Select selectFilter = new Select(driver.findElement(By.xpath(
-                "//div[contains(@class, 'index-content')]" +
-                        "//select[contains(@class, 'select-select') and position()=1]")));
-
-        selectFilter.selectByVisibleText("Дороже");
+        filter.setFilter("Дороже");
 
         //Шаг 8
         //В List будут находится найденные элементы
