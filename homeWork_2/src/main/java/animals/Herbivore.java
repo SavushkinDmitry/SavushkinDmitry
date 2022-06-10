0c9cd041551abfc91cd8a97fc5ac66b2dd662a53
Savushkin Dmitry <wexler.kill@mail.ru>
2022-06-10 20:22:38 +0300

    Fix code

--- homeWork_2/src/main/java/animals/Herbivore.java
+++ homeWork_2/src/main/java/animals/Herbivore.java
@@ -16,10 +16,17 @@ public class Herbivore extends Animal {
         if (_food instanceof Grass) {
             System.out.println(getNameAnimals() + " will eat " + _food.getName());
             addSatiety(_food);
+<<<<<<< Updated upstream
             System.out.println("Прибавлено сытости: " + getSatiety());
         } else {
             System.out.println(getNameAnimals() + " don't will eat " + _food.getName());
             System.out.println("Сытость " + getNameAnimals() + ": " + getSatiety());
+=======
+            System.out.println("Satiety is added: " + getSatiety());
+        } else {
+            System.out.println(getNameAnimals() + " don't will eat " + _food.getName());
+            System.out.println("Satiety " + getNameAnimals() + ": " + getSatiety());
+>>>>>>> Stashed changes
         }
     }
 
